a08487281e75cee141dca7108891a4c018b894d0
moskalec <moskalec.d@gmail.com>
2018-06-04 22:37:41 +0300

    Update models

--- blog/models.py
+++ blog/models.py
@@ -2,18 +2,59 @@ from django.db import models
 from django.utils import timezone
 
 
-class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+class User(models.Model):
+    name = models.CharField(max_length=80)
 
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
 
-    def __str__(self):
-        return self.title
+class Category(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    image = models.ImageField(height=100, width=100)
+    description = models.CharField(max_length=200)
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(blank=True, null=True)
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(height=700, width=700)
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(blank=True, null=True)
+    publication_date = models.DateTimeField(blank=True, null=True)
+    visible = models.BooleanField(default=False)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(blank=True, null=True)
+    articles = models.ManyToManyField('Article')
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=1000)
+    created = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    article = models.ForeignKey('Article', on_delete=models.CASCADE)
+
+
+#
+#class Post(models.Model):
+#    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+#    title = models.CharField(max_length=200)
+#    text = models.TextField()
+#    created_date = models.DateTimeField(
+#            default=timezone.now)
+#    published_date = models.DateTimeField(
+#            blank=True, null=True)
+#
+#    def publish(self):
+#        self.published_date = timezone.now()
+#        self.save()
+#
+#    def __str__(self):
+#        return self.title
--- blog/urls.py
+++ blog/urls.py
@@ -8,3 +8,21 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/(?P<pk>\d+)/edit/', views.post_edit, name='post_edit'),
 ]
+
+#Urls
+#    /
+#    /categories/
+#    /categories/<category_title>/
+#    /categories/<category_title>/articles/
+#    /categories/<category_title>/articles/<article_title>/
+#    /tags/
+#    /tags/<tag_title>/
+#
+#    Requirements:
+#    1. HRU only
+#    2. Transliterated urls
+#        e.g.:
+#            category.title = 'спорт'
+#            articler.title = 'Финал Лиги Чемпионов все'
+#
+#            url => /categories/sport/articles/final-ligi-chempionov-vse/
